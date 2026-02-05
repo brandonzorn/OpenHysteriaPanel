77c29603993ac98e4fa28b024b1e887d647490f2
bzorn <v.klimenko.2137@gmail.com>
2026-02-05 23:46:19 +0300

    fix: use view names in sidebar urls

--- hystpanel/clients/urls.py
+++ hystpanel/clients/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from clients.views import ClientsView
 
+app_name = "clients"
+
 urlpatterns = [
-    path("", ClientsView.as_view(), name="clients"),
+    path("", ClientsView.as_view(), name="clients-list"),
 ]
--- hystpanel/dashboard/urls.py
+++ hystpanel/dashboard/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import DashboardView
 
+app_name = "dashboard"
+
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
 ]
