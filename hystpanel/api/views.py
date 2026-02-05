90db69733df22c9c7f951ae6143f67281b2083c6
bzorn <v.klimenko.2137@gmail.com>
2026-02-05 15:00:27 +0300

    test: add request logger

--- hystpanel/api/views.py
+++ hystpanel/api/views.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 import logging
 
 from rest_framework import status
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -46,6 +46,8 @@ class ServerControlView(APIView):
 
 
 class HysteriaAuthView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         logger.info("Hysteria auth request data: %s", request.data)
         auth_value = request.data.get("auth")
