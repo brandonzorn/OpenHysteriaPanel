4b99dee3f369a320f9d3726fb1bc3ed1ae8db57a
bzorn <v.klimenko.2137@gmail.com>
2026-02-05 14:08:41 +0300

    fix: change database dir

--- hystpanel/hystpanel/settings.py
+++ hystpanel/hystpanel/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "hystpanel.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "etc" / "db.sqlite3",
+        "NAME": BASE_DIR / "sqlite" / "db.sqlite3",
     },
 }
 
